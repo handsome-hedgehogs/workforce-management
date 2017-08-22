14c430d47cb2858ac79b312f79a33a60a20dcf19
Tamela Lerma <Tlerma83@gmail.com>
2017-08-22 14:19:56 -0700

    added trainingProgram details

---
+++ HandsomeHedgehogHoedown/ViewModels/EmployeeTraingingViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HandsomeHedgehogHoedown.Models;
+
+namespace HandsomeHedgehogHoedown.ViewModels
+{
+    public class EmployeeTrainingViewModel
+    {
+        public TrainingProgram tp { get; set; }
+        public List<Employee> Employees {get; set;}
+
+        public EmployeeTrainingViewModel()
+        {
+            Employees = new List<Employee>();
+        }
+
+    }
+}
