a6821ce27716636d0d524d472f17a6efafeeafee
Tamela Lerma <Tlerma83@gmail.com>
2017-08-22 16:30:27 -0700

    documentation

--- HandsomeHedgehogHoedown/Controllers/TrainingProgramsController.cs
+++ HandsomeHedgehogHoedown/Controllers/TrainingProgramsController.cs
@@ -27,6 +27,9 @@ namespace HandsomeHedgehogHoedown.Controllers
         }
 
         // GET: TrainingPrograms/Details/5
+        // insert viewModel EmplyoyeeTrainingViewModel object insance
+        // ViewModel includes object instance of List<Employee> to add Employee Object that is assigned to a Training Program
+        // Authored by : Tamela Lerma
         public async Task<IActionResult> Details(int? id)
         {
             EmployeeTrainingViewModel employeesTP = new EmployeeTrainingViewModel();
@@ -36,20 +39,26 @@ namespace HandsomeHedgehogHoedown.Controllers
                 return NotFound();
             }
 
+            // use view model instance, call instance of training program model within ViewModel
+            // call TP Model, Include Icollection join table EmployeeTrainings
+            // get Single object that has TrainingProgramId that is equal to id that is passed as argument
+            // this will list details for a single training program  that is selected
+            // Authored by : Tamela Lerma
             employeesTP.tp = await _context.TrainingProgram.Include(et => et.EmployeeTrainings)
                 .SingleOrDefaultAsync(m => m.TrainingProgramId == id);
-
+            // iterate through ICollection of EmployeeTrainings
             foreach (var item in employeesTP.tp.EmployeeTrainings) {
+                // create a var of type employee and query Employee table to find EmployeeID that matches an EmployeeID from joined table in ICollection
                 Employee employee = await _context.Employee.SingleOrDefaultAsync(e => e.EmployeeId == item.EmployeeId);
+                // Once employee object is selected, add to List<Employee> Employees on the ViewModel
                 employeesTP.Employees.Add(employee);
             }
-
 
             if (employeesTP.tp == null)
             {
                 return NotFound();
             }
-
+            // pass ViewModel instance that Includes Joined Table Collection
             return View(employeesTP);
         }
 
--- HandsomeHedgehogHoedown/ViewModels/EmployeeTraingingViewModel.cs
+++ HandsomeHedgehogHoedown/ViewModels/EmployeeTraingingViewModel.cs
@@ -4,13 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using HandsomeHedgehogHoedown.Models;
 
+// Class that is a ViewModel for TrainingProgramController and TrainingProgram/Views/Details
+// Creates instance of TrainingProgram Model
+// Create List<Employee> to add Employee objects that match query in TrainingProgramController Detail Method
+// Authored by : Tamela Lerma
 namespace HandsomeHedgehogHoedown.ViewModels
 {
     public class EmployeeTrainingViewModel
     {
+        // Create instance of TrainingProgram Model to pass into Controller
         public TrainingProgram tp { get; set; }
+        // Create List to add Employees that match query to
         public List<Employee> Employees {get; set;}
-
+        // use constructor method to creat object instance of List<Employee>
         public EmployeeTrainingViewModel()
         {
             Employees = new List<Employee>();
